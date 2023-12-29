5894cb070d441489321210d23a4d6154ca216412
balusena <balusena12@gmail.com>
2023-12-29 22:09:32 +0000

    roboshop aws parameters

--- main.tf
+++ main.tf
@@ -7,7 +7,10 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
   default  = [
-    { name = "demo1", value = "demo1", type = "String" },
-    { name = "demo2", value = "demo2", type = "String" }
+    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.robobal.store", type = "String" },
+    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.robobal.store", type = "String" },
+    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.robobal.store", type = "String" },
+    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.robobal.store", type = "String" },
+    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.robobal.store", type = "String" }
   ]
 }
