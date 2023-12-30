3eebb064801cff247551291d16bfbd96225b529a
balusena <balusena12@gmail.com>
2023-12-30 13:14:17 +0000

    roboshop ansible files

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
   default  = [
-    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.robobal.store:8080", type = "String" },
-    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.robobal.store:8080", type = "String" },
-    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.robobal.store:8080", type = "String" },
-    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.robobal.store:8080", type = "String" },
-    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.robobal.store:8080", type = "String" }
+    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.robobal.store:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.robobal.store:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.robobal.store:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.robobal.store:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.robobal.store:8080/", type = "String" }
   ]
 }
